b0893761e2c4e95c1ac996744a8a0f6ff3c96de5
Silvestre Cuellar <cuellarsilvestre@gmail.com>
2021-06-27 12:34:45 -0700

    can rob ships

--- t_myth-creatures/lib/pirate.rb
+++ t_myth-creatures/lib/pirate.rb
@@ -8,6 +8,7 @@ class Pirate
         @name = name
         @job = job
         @cursed_count = 0
+        @booty = 0
     end 
 
     def commit_heinous_act
@@ -20,5 +21,13 @@ class Pirate
         else
             false
         end  
+    end 
+
+    def booty
+        @booty
+    end 
+
+    def rob_ship
+        @booty += 100
     end 
 end 
